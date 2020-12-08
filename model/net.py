b0d50eaa9adab8a4ff83e654dcf5b95f70a48de6
Irrmm <grazashar@mail.ru>
2020-12-08 22:27:18 +0200

    Add SiameseNet

--- model/net.py
+++ model/net.py
@@ -12,23 +12,26 @@ class Flatten(nn.Module):
 
 
 class TwinNet(nn.Module):
-    def __init__(self, in_channels: int):
+    def __init__(self, in_channels: int, out_size: int):
         super().__init__()
         self.conv_block1 = self._create_conv_block(in_channels, 64, conv_kernel_size=(10, 10))
         self.conv_block2 = self._create_conv_block(64, 128, conv_kernel_size=(10, 10))
         self.conv_block3 = self._create_conv_block(128, 128, conv_kernel_size=(4, 4))
-        self.final_block = nn.Sequential(
+        self.conv_block4 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=(4, 4), stride=1),
-            nn.ReLU(),
+            nn.ReLU()
+        )
+        self.fc_block = nn.Sequential(
             Flatten(),
-            nn.Linear(in_features=256 * 256, out_features=266 * 256)
+            nn.Linear(in_features=256 * 256, out_features=out_size)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         result = self.conv_block1(x)
         result = self.conv_block2(result)
         result = self.conv_block3(result)
-        result = self.final_block(result)
+        result = self.conv_block4(result)
+        result = self.fc_block(result)
         return result
 
     def _create_conv_block(self,
@@ -42,3 +45,17 @@ class TwinNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=pool_kernel_size, stride=2),
         )
+
+
+class SiameseNet(nn.Module):
+    def __init__(self, in_channels: int, twin_output_size: int):
+        super().__init__()
+        self.twin1 = TwinNet(in_channels, twin_output_size)
+        self.twin2 = TwinNet(in_channels, twin_output_size)
+        self.combine_block = nn.Sequential(
+            nn.Linear(in_features=twin_output_size, out_features=1),
+            nn.Softmax(),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.combine_block(torch.abs(self.twin1(x) - self.twin2(x)))
